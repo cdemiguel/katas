e42ee956190662b6cb6f02b91ef168f60b029be4
cdemiguel <demiguelferrerocarlos@gmail.com>
2018-05-07 21:21:40 +0200

    refactor naming and semantic

--- replace/test/test.js
+++ replace/test/test.js
@@ -4,18 +4,18 @@ const assert = require('assert')
 
 
 // en un texto una palabra que yo le paso se ha de hacer replace por tantas XXXX como la palabra tenga
-describe('Replace', function() {
+describe('Censor word', function() {
 
     let text = 'the quick brown fox jumps over the lazy dog'
 
-    it('should replace the word fox for how many xs have the word', function() {
+    it('should replace the word fox for how many xs has the word', function() {
       let word = 'fox'
       assert.equal(replace.wordInASentence(word, text), 'the quick brown XXX jumps over the lazy dog');
     }),
 
     it('should replace the words in array for how many xs have the words', function(){
-      let words = ['fox', 'dog']
-      assert.equal(replace.wordInASentence(words, text), 'the quick brown XXX jumps over the lazy XXX');
+      let words = ['fox', 'dog', 'quick']
+      assert.equal(replace.wordInASentence(words, text), 'the XXXXX brown XXX jumps over the lazy XXX');
     })
 
 });
